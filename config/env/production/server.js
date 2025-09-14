3b986b334a0033767f7f1769b74cb7c9c724dc07
Oleksandr Tymofyeyev <oleksandr.tymofyeyev@eze.tech>
2025-09-14 11:08:19 +0300

    proxy

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 // Path: ./config/env/production/server.js`
 export default ({ env }) => ({
     proxy: true,
-    url: env('MY_HEROKU_URL', 'https://amcred-server-de997fc648f5.herokuapp.com/'), // Sets the public URL of the application.
+    url: env('MY_HEROKU_URL', 'https://amcred-server-de997fc648f5.herokuapp.com'), // Sets the public URL of the application.
     app: { 
          keys: env.array('APP_KEYS')
        },
