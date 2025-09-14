d961254f455a01f91551e5e55d0a7566fec6d7ae
Oleksandr Tymofyeyev <oleksandr.tymofyeyev@eze.tech>
2025-09-14 11:20:46 +0300

    proxy

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,31 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  // 'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['https://amcred2-client.vercel.app'], // Add your frontend domain(s)
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      enabled: true,
+      // Other session configurations if needed
+      // Ensure secure is set based on environment if not handled by proxy trust
+      secure: env('NODE_ENV') === 'production',
+      proxy: true, // Explicitly enable proxy mode for the session middleware
+    },
+  },
+  // 'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
